5bacd7d122b1a768d20951067b1528e2ac0ad53a
Jorg Pleumann <>
2009-03-24 19:40:24 -0700

    Automated import from //branches/master/...@141154,141154

--- stubdata/helper.sh
+++ stubdata/helper.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 export TOOLS=$ANDROID_EABI_TOOLCHAIN/../../../icu-3.8
+export WHICH=$1
+
+if [ "$WHICH" == "" ]; then
+    echo "Usage: ./helper <config name>"
+    exit
+fi;
+
+if [ ! -d "cfg-$WHICH" ]; then
+    echo "Configuration $WHICH does not exist."
+    exit
+fi;
+
+cp cfg-$WHICH/icudt38l/res_index.txt icudt38l
+cp cfg-$WHICH/icudt38l/brkitr/res_index.txt icudt38l/brkitr
+cp cfg-$WHICH/icudt38l/coll/res_index.txt icudt38l/coll
+cp cfg-$WHICH/icudt38l/rbnf/res_index.txt icudt38l/rbnf
 
 echo "Compiling (possibly modified) source files into binaries..."
 cd icudt38l
@@ -20,6 +36,8 @@ $TOOLS/genrb res_index.txt
 cd ../..
 
 echo "Creating ICU data file..."
-$TOOLS/icupkg -tl -s icudt38l -a icudt38l.txt new icudt38l.dat
+$TOOLS/icupkg -tl -s icudt38l -a cfg-$WHICH/icudt38l.txt new icudt38l.dat
+cp icudt38l.dat icudt38l-$WHICH.dat
+rm icudt38l.dat
 
 echo "Finished."
