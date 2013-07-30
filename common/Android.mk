2e308945ed3e9792a01d49c3bb2d6238fdb5d671
Elliott Hughes <enh@google.com>
2013-07-29 18:55:13 -0700

    Switch to using libstlport.mk and use_rtti.mk.
    Change-Id: I24244bc811e6cdd4b09f30546283b9808550d3cb

--- common/Android.mk
+++ common/Android.mk
@@ -143,16 +143,15 @@ local_ldlibs := -ldl -lm -lpthread
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(src_files)
-LOCAL_C_INCLUDES := $(c_includes)
-LOCAL_C_INCLUDES += abi/cpp/include # RTTI
-LOCAL_C_INCLUDES += external/stlport/stlport bionic/ bionic/libstdc++/include # STL
+LOCAL_C_INCLUDES += $(c_includes)
 LOCAL_CFLAGS := $(local_cflags) -DPIC -fPIC
-LOCAL_RTTI_FLAG := -frtti
-LOCAL_SHARED_LIBRARIES += libgabi++ libdl libstlport
+LOCAL_SHARED_LIBRARIES += libdl
 LOCAL_LDLIBS += $(local_ldlibs)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libicuuc
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
+include abi/cpp/use_rtti.mk
+include external/stlport/libstlport.mk
 include $(BUILD_SHARED_LIBRARY)
 
 
